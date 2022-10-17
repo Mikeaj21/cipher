9aa5b39e99e788bc5901912967f4fe72827022e4
Mikeaj21 <113301897+Mikeaj21@users.noreply.github.com>
2022-10-17 14:26:43 -0700

    Add files via upload

---
+++ function runtimer.js
@@ -0,0 +1,68 @@
+function runtimer(x){
+    setTimeout(function(){
+    x.innerHTML=Currtime;
+    Currtime=Currtime-5;
+}, timeout);
+timeout=timeout+1000;
+
+setTimeout(function(){
+    x.innerHTML=Currtime;
+    Currtime=Currtime-5;
+}, timeout);
+timeout=timeout+1000;
+setTimeout(function(){
+    x.innerHTML=Currtime;
+    Currtime=Currtime-5;
+}, timeout);
+timeout=timeout+1000;
+setTimeout(function(){
+    x.innerHTML=Currtime;
+    Currtime=Currtime-5;
+}, timeout);
+timeout=timeout+1000;
+setTimeout(function(){
+    x.innerHTML=Currtime;
+    Currtime=Currtime-5;
+}, timeout);
+timeout=timeout+1000;
+setTimeout(function(){
+    x.innerHTML=Currtime;
+    Currtime=Currtime-5;
+}, timeout);
+timeout=timeout+1000;
+setTimeout(function(){
+    x.innerHTML=Currtime;
+    Currtime=Currtime-5;
+}, timeout);
+timeout=timeout+1000;
+setTimeout(function(){
+    x.innerHTML=Currtime;
+    Currtime=Currtime-5;
+}, timeout);
+timeout=timeout+1000;
+
+setTimeout(function(){
+    x.innerHTML=Currtime;
+    Currtime=Currtime-5;
+}, timeout);
+timeout=timeout+1000;
+setTimeout(function(){
+    x.innerHTML=Currtime;
+    Currtime=Currtime-5;
+}, timeout);
+timeout=timeout+1000;
+setTimeout(function(){
+    x.innerHTML=Currtime;
+    alert("BLASTOFF!!");
+}, timeout);
+timeout=timeout+1000;
+
+
+
+
+
+
+
+
+} function startbuttonclick(){}
+function stopbuttonclick(){}
